d4b735f540d2bb23ec170cf887163d6c10822260
Unknown <103126971+HE110W0R19@users.noreply.github.com>
2024-04-19 16:19:41 +0300

    Add new  json parser class
    Added the JsonUtilities class so that you can extract the "connection string" data from a file

---
+++ CarStoreWEB/CarStoreDataAccess/EntityModels/FirstTestDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using CarStoreDataAccess.JsonUtilities;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace CarStoreWEB.EntityModels;
+
+public partial class FirstTestDbContext : DbContext
+{
+    //Connection String getter
+    private readonly JsonUtilities JsonParser = new JsonUtilities();
+
+    public FirstTestDbContext()
+    {
+    }
+
+    public FirstTestDbContext(DbContextOptions<FirstTestDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseMySql(JsonParser.GetConnectionStr(@"..\ConnectionString.json"), 
+            Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.3.0-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    
+}
---
+++ CarStoreWEB/CarStoreDataAccess/JsonParser/JsonUtilities.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace CarStoreDataAccess.JsonUtilities
+{
+    public class JsonUtilities
+    {
+        [JsonProperty("DbConnectionString")]
+        public string? ConnectionStrValue { get; set; }
+
+        /// <summary>
+        /// Method for geting connection string from ".json" file
+        /// </summary>
+        /// <param name="path">Is path to JSON file</param>
+        /// <returns></returns>
+        public string GetConnectionStr(string path)
+        {
+            /* 
+             Use .json format like this ->
+             {
+                "DbConnectionString": "*your connection string*"
+             }
+            */
+
+            var json = File.ReadAllText(path);
+            var data = JsonConvert.DeserializeObject<JsonUtilities>(json);
+
+            if (string.IsNullOrEmpty(data?.ConnectionStrValue))
+            {
+                return "Connection string is not found of empty.";
+            }
+            return data.ConnectionStrValue;
+        }
+    }
+}
